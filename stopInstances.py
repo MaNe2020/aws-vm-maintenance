2a2d2a744247f22a4797fdc4cf2e8df1c313cbcd
Gurunath <guru9794@gmail.com>
2021-03-22 19:12:42 +0530

    Updated alertbot instance id

--- stopInstances.py
+++ stopInstances.py
@@ -1,9 +1,14 @@
 import boto3
 from botocore.config import Config
+import requests
 
 def lambda_handler(event, context):
     
     prodInstancesList = []
+    instanceTxt = 'https://raw.githubusercontent.com/k8-proxy/aws-vm-maintenance/main/instances.txt'
+    r = requests.get(instanceTxt)
+    with open('instances.txt', 'wb') as f:
+        f.write(r.content)
     with open('instances.txt') as f:
         instanceList = f.read().splitlines()
     for instanceId in instanceList:
@@ -38,4 +43,6 @@ def lambda_handler(event, context):
                     print (instance['InstanceId'] + " - NOT To be stopped")
         if(len(tobeStoppedInstances) > 0):
             print("tobeStopped Instances: "+ str(tobeStoppedInstances))
-            ec2client.stop_instances(InstanceIds=tobeStoppedInstances)
+            #ec2client.stop_instances(InstanceIds=tobeStoppedInstances)
+if __name__ == "__main__":
+    lambda_handler("test","test")
